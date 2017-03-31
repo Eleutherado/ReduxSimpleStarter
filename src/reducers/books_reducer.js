0583522c1ccf513e343c9e3025c7ca0d5b9d1805
Eleutherado <emiliov432@gmail.com>
2017-03-31 02:05:43 -0400

    Added books reducer, book list container and rendered them

---
+++ src/reducers/books_reducer.js
@@ -0,0 +1,8 @@
+export default () =>{
+    return [
+        {title: "The Perennial Philosophy"},
+        {title: "A Dance of Dragons"},
+        {title: "A Brave New World"},
+        {title: "El Principito"},
+    ]
+}
